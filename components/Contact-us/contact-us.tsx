8ed1352c4181525c43f0777f02728bdc96df4cc5
Bayodele Shedu <shedubayode@gmail.com>
2024-05-04 11:47:07 +0100

    personals

--- components/Contact-us/contact-us.tsx
+++ components/Contact-us/contact-us.tsx
@@ -15,61 +15,53 @@ declare global {
 }
 
 export default function ContactPage() {
-  const [formData, setFormData] = useState({
+  const [form, setForm] = useState({
     fullName: "",
     message: "",
     email: "",
   });
 
   useEffect(() => {
-  const mapContainer = document.getElementById('map');
-  if (!mapContainer) return;
+    const mapContainer = document.getElementById("map");
+    if (!mapContainer) return;
 
-  if (!mapContainer.hasChildNodes()) {
-    const map = L.map("map").setView([37.6975, -97.3698], 10);
+    if (!mapContainer.hasChildNodes()) {
+      const map = L.map("map").setView([37.6975, -97.3698], 10);
 
-    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-      attribution: "&copy; OpenStreetMap contributors",
-    }).addTo(map);
+      L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution: "&copy; OpenStreetMap contributors",
+      }).addTo(map);
 
-    // Replace the latitude and longitude with your street address
-    const address = "6820 W Central Ave, Wichita, KS 67212";
-    const geocodeUrl = `https://nominatim.openstreetmap.org/search?q=${address}&format=json&addressdetails=1&limit=1`;
+      const geocodeUrl = `https://nominatim.openstreetmap.org/search?q=6820+W+Central+Ave,+Wichita,+KS+67212&format=json&addressdetails=1&limit=1`;
 
-    fetch(geocodeUrl)
-      .then(response => response.json())
-      .then(data => {
-        const { lat, lon } = data[0];
-        map.setView([lat, lon], 20);
-        L.marker([lat, lon]).addTo(map).bindPopup(address).openPopup();
-      })
-      .catch(error => {
-        console.error('Error fetching geocoding data:', error);
-      });
-  }
-}, []);
-
+      fetch(geocodeUrl)
+        .then((response) => response.json())
+        .then((data) => {
+          const { lat, lon } = data[0];
+          map.setView([lat, lon], 20);
+          L.marker([lat, lon])
+            .addTo(map)
+            .bindPopup("6820 W Central Ave, Wichita, KS 67212")
+            .openPopup();
+        })
+        .catch(console.error);
+    }
+  }, []);
 
   const handleChange = (e: any) => {
     const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
+    setForm({ ...form, [name]: value });
   };
 
   const handleSubmit = (e: any) => {
     e.preventDefault();
     emailjs
-      .send(
-        "service_aug4hyu",
-        "template_ff4yied",
-        formData,
-        "mRm23xSD-WMIu8ZDK"
-      )
-      .then((response) => {
-        console.log("SUCCESS!", response.status, response.text);
+      .send("service_aug4hyu", "template_ff4yied", form, "mRm23xSD-WMIu8ZDK")
+      .then(() =>
         toast.success("Your message has been sent successfully!", {
           theme: "colored",
-        });
-      })
+        })
+      )
       .catch(() =>
         toast.error("Something went wrong! Please try again later.", {
           theme: "colored",
@@ -85,20 +77,16 @@ export default function ContactPage() {
           <h1 className="text-3xl font-bold text-green-600 text-center mb-6">
             Contact Us
           </h1>
-          {/* <p className="text-center mb-6">
-            Have questions? We're here to help! Please fill out the form below
-            and we'll get back to you as soon as possible.
-          </p> */}
           <div className="lg:grid lg:grid-cols-2">
             <div className="mb-6">
               <p className="font-bold text-2xl mb-4">Contact Information:</p>
-              <div className="flex items-center mb-2 gap-2 ">
+              <div className="flex items-center mb-2 gap-2">
                 <div className="bg-green-600 rounded-full w-8 h-8 p-2 flex items-center justify-center">
                   <FiMapPin className=" " />
                 </div>
-                <p>Physical Address: Your Address Here</p>
+                <p>Physical Address: 6820 W Central Ave, Wichita, KS 67212</p>
               </div>
-              <div className="flex just mb-2 gap-2">
+              <div className="flex items-center mb-2 gap-2">
                 <div className="bg-green-600 rounded-full p-2 w-8 h-8 flex items-center justify-center">
                   <FiPhone className="" />
                 </div>
@@ -137,15 +125,15 @@ export default function ContactPage() {
               </div>
               <form onSubmit={handleSubmit}>
                 <div className="mb-6">
-                  <label className="block  font-bold mb-2" htmlFor="name">
+                  <label className="block  font-bold mb-2" htmlFor="fullName">
                     Your FullName
                   </label>
                   <input
                     className="appearance-none text-black font-inter border rounded w-full py-2 px-3  leading-tight focus:outline-none focus:shadow-outline"
-                    id="name"
+                    id="fullName"
                     type="text"
                     name="fullName"
-                    value={formData.fullName}
+                    value={form.fullName}
                     onChange={handleChange}
                     placeholder="Your FullName"
                   />
@@ -159,7 +147,7 @@ export default function ContactPage() {
                     id="email"
                     type="email"
                     name="email"
-                    value={formData.email}
+                    value={form.email}
                     onChange={handleChange}
                     placeholder="Your Email"
                   />
@@ -172,7 +160,7 @@ export default function ContactPage() {
                     className="appearance-none border rounded text-black w-full py-2 px-3  leading-tight focus:outline-none focus:shadow-outline h-32 resize-none"
                     id="message"
                     name="message"
-                    value={formData.message}
+                    value={form.message}
                     onChange={handleChange}
                     placeholder="Your Message"
                   ></textarea>
